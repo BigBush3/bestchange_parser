34a1ac05b921c9aa322dbe8d61ac6863ed51ce53
reszi123 <imrezz07@gmail.com>
2021-11-05 19:16:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
+import os
 
 app = Flask(__name__)
 
@@ -9,15 +10,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     resp = urlopen('http://api.bestchange.ru/info.zip')
-    zipfile = ZipFile(BytesIO(resp.read()))
-    data = zipfile.read('bm_rates.dat')
-    with open(data) as zipfile:
-        for line in zipfile:
-            line_list = line.strip('\n').split(';')
-            print(f'{float (line_list [0])}   {float (line_list [1]):.5f}')
-
+    zipFile = ZipFile(BytesIO(resp.read()))
+    zipFile.extract('bm_rates.dat')
+    with open("bm_rates.dat", "r", encoding="windows-1251") as data_file:
+        lst = []
+        for line in data_file:
+            lst.append(line.strip().split(';'))
+        print(lst[0][0])
+    os.remove("./bm_rates.dat")
+        
     return "Hello, World!"
-
 
 if __name__ == "__main__":
     app.run(debug=True)
