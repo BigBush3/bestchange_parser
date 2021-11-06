76b4bde701ce3757f21f1f4559ed2a0a5528caea
Andrey <andrey.platonov2005@gmail.com>
2021-11-06 22:09:22 +0300

    sth

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
@@ -8,8 +8,10 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["POST"])
 def index():
+    input_json = request.get_json(force=True)
+    print(input_json)
     resp = urlopen('http://api.bestchange.ru/info.zip')
     zipFile = ZipFile(BytesIO(resp.read()))
     zipFile.extract('bm_rates.dat')
@@ -17,32 +19,37 @@ def index():
         lst = []
         for line in data_file:
             lst.append(line.strip().split(';'))
-    getSell = "BTC"
-    getBuy = "ETH"
+    
     with open('valueArray.json') as f:
         value = json.load(f)
-        idSell = int("".join([str(i["id"])
-                              for i in value if i["key"] == getSell]))
-        idBuy = int("".join([str(i["id"])
-                            for i in value if i["key"] == getBuy]))
-        strValue = []
-        for i in lst:
-            if int(i[0]) == idSell and int(i[1]) == idBuy:
-                strValue.append(i)
-        for i in strValue:
-            if float(i[3]) > float(i[4]):
-                sortedStrValueDown = sorted(
+        totalDir = {}
+        for item in input_json:
+            
+            idSell = int("".join([str(i["id"])
+                                for i in value if i["key"] == item["give"]]))
+            idBuy = int("".join([str(i["id"])
+                                for i in value if i["key"] == item["get"]]))
+            strValue = []
+            
+            for i in lst:
+                if int(i[0]) == idSell and int(i[1]) == idBuy:
+                    strValue.append(i)
+            for i in strValue:
+                if float(i[3]) > float(i[4]):
+                    sortedStrValueDown = sorted(
                     strValue, key=lambda x: (-float(x[4]), float(x[3])))
-                print(sortedStrValueDown)
-                break
-            elif float(i[3]) < float(i[4]):
-                sortedStrValueUp = sorted(
-                    strValue, key=lambda x: (-float(x[3]), float(x[4])))
-                print(sortedStrValueUp)
-                break
+                    sortedStrValueDown = [i[3 : 5] for i in sortedStrValueDown[:3]]
+                    totalDir[item["give"] + '-' + item["get"]] = sortedStrValueDown
+                    break
+                elif float(i[3]) < float(i[4]):
+                    sortedStrValueUp = sorted(
+                    strValue, key=lambda x: (-float(x[4]), float(x[3])))
+                    sortedStrValueUp = [i[3 : 5] for i in sortedStrValueUp[:3]]
+                    totalDir[item["give"] + '-' + item["get"]] = sortedStrValueUp
+                    break
     os.remove("./bm_rates.dat")
-
-    return "Hello, World!"
+    return jsonify(totalDir)
+    
 
 
 if __name__ == "__main__":
